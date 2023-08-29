d4a695bff89e73b716c016d40d16a1ace93bff51
PrimerCodigo <133933376+PrimerCodigo@users.noreply.github.com>
2023-08-28 22:03:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,13 +63,19 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-#New Section to display fruityvice api response
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlist.text(back_from_function)
+
 
 streamlit.stop()
-
-#This will not work correctly, but just go whith it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html
